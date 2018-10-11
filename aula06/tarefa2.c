12c3dc38d678978fda29b39a5ce98400f14dc4cf
Luciana <lucianadacostamarques@gmail.com>
2018-10-11 11:10:46 -0300

    started task 2

--- aula06/tarefa2.c
+++ aula06/tarefa2.c
@@ -5,6 +5,9 @@
 
 int X[SIZE];
 int Y[SIZE];
+int A[SIZE];
+int B[SIZE];
+int C[SIZE];
 
 int main()
 {
@@ -15,6 +18,35 @@ int main()
     {
       X[i] = i;
       Y[i] = i + 1;
+    }
+  }
+  #pragma omp sections
+  {
+    int i,j;
+    #pragma omp section
+    {
+      for (i = 0; i < SIZE - 1; i++)
+      {
+        A[i] = i*3 + 15;
+      }
+    }
+    #pragma omp section
+    {
+      B[0] = 1;
+      int k, l;
+      for (k = 1; k < SIZE; k ++)
+      {
+        B[0] = Y[i] + A[i-1];
+      }
+    }
+    #pragma omp section
+    {
+      C[0] = 1;
+      int x,w;
+      for (x = 1; x < SIZE; x++)
+      {
+        C[i] = Y[i] + B[i-1]*2;
+      }
     }
   }
   return 0;
