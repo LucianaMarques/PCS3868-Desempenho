1fc36be12385b41049db66614d1c2cb50ffb4fc9
luciana <lucianadacostamarques@gmail.com>
2018-11-01 11:50:25 -0300

    ex02 not so well

--- aula09/busca-seq.c
+++ aula09/busca-seq.c
@@ -1,4 +1,4 @@
-// Exercicio 1: Programa busca uma sequencia, definida pelo usuario, em um arquivo com varias sequencias 
+// Exercicio 1: Programa busca uma sequencia, definida pelo usuario, em um arquivo com varias sequencias
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,60 +6,117 @@
 #define TRUE 0
 #define FALSE 1
 
-int main()
+main(argc, argv)
+
+int			argc;
+char			*argv[];
 {
-    int i = 0;
-    int j;
-    int *indice_achou;
-    char * buff;
-    char sequencia[SEQ_SIZE];
-    int file_size;
-    char ** seq_vet;
-    int n_seq;
-    int k;
-    FILE *f;
-    f = fopen("seq-teste.txt", "r");
-    fseek(f,0L,SEEK_END);
-    file_size=ftell(f);
-    fseek(f,0L,SEEK_SET);
-    n_seq=(file_size+1)/(SEQ_SIZE-1);
-    seq_vet=(char **)malloc(n_seq*sizeof(char *));
-    for (k=0;k<n_seq;k++)
-        seq_vet[k]=malloc(SEQ_SIZE*sizeof(char));
-    indice_achou=(int *) malloc (n_seq * sizeof(int));
-    for (i=0;i<n_seq;i++){
-	fgets(seq_vet[i], SEQ_SIZE, f);
-        printf("seq_vet[%d] =  %s\n ",i,seq_vet[i]);
-   }
-        
-   printf("file_size=%d\n",file_size);
+		int		n,n_nos, rank;
+		MPI_Status	status;
+		/*
+		 * Initialize MPI.
+		 */
+			MPI_Init(&argc, &argv);
+		/*
+		 * Error check the number of processes.
+		 * Determine my rank in the world group.
+		 * The sender will be rank 0 and the receiver, rank 1.
+		 */
+		MPI_Comm_size(MPI_COMM_WORLD, &n_nos);
+		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   printf("Digite a sequencia que deseja buscar: ");
-   scanf("%s",&sequencia);
-   k=0;
-   for (i=0;i<n_seq;i++){	  
-        buff=seq_vet[i];
-        printf("i=%d buff %s\n ",i,buff);
-        for (j = 0; j < SEQ_SIZE-2; j++){
-		if (sequencia[j] != buff[j]){
-			break;
-		}
+		int i = 0;
+		int j;
+		int *indice_achou;
+		char * buff;
+		char sequencia[SEQ_SIZE];
+		int file_size;
+		char ** seq_vet;
+		int n_seq;
+		int k;
+		FILE *f;
+
+		if (rank == 0){
+
+		/*
+		*
+		* LEITURA DO ARQUIVO
+		*
+		*/
+
+			f = fopen("seq-teste.txt", "r");
+			fseek(f,0L,SEEK_END);
+			file_size=ftell(f);
+			fseek(f,0L,SEEK_SET);
+			n_seq=(file_size+1)/(SEQ_SIZE-1);
+
+			seq_vet=(char **)malloc(n_seq*sizeof(char *));
+
+			for (k=0;k<n_seq;k++)
+				seq_vet[k]=malloc(SEQ_SIZE*sizeof(char));
+
+			indice_achou=(int *) malloc (n_seq * sizeof(int));
+
+			for (i=0;i<n_seq;i++){
+				fgets(seq_vet[i], SEQ_SIZE, f);
+				printf("seq_vet[%d] =  %s\n ",i,seq_vet[i]);
+			}
+
+			printf("file_size=%d\n",file_size);
+			printf("Digite a sequencia que deseja buscar: ");
+			scanf("%s",&sequencia);
+
+			MPI_Send(void *buf,int count,MPI_Datatype dtype,int dest,int tag,MPI_Comm comm);
+
+
+			k=0;
+			for (i=0;i<n_seq;i++){
+						buff=seq_vet[i];
+						printf("i=%d buff %s\n ",i,buff);
+						for (j = 0; j < SEQ_SIZE-2; j++){
+								if (sequencia[j] != buff[j]){
+										break;
+							  }
+						}
+						if (j== SEQ_SIZE-2) {
+								indice_achou[k]=i;
+								k++;
+						}
+			}
+			if (k != 0){
+				printf("Sequencia encontrada  %d vezes\n",k);
+				for (i=0;i<k;i++)
+						printf("i=%d  ",indice_achou[i]);
+				printf ("\n");
+			}
+
+			else
+				printf("Sequencia nao encontrada\n ");
 	}
-        if (j== SEQ_SIZE-2) {
-              indice_achou[k]=i;
-              k++;
-        }
-   }
 
-   if (k != 0){
-	printf("Sequencia encontrada  %d vezes\n",k);
-        for (i=0;i<k;i++)
-           printf("i=%d  ",indice_achou[i]);
-        printf ("\n");
-        }
-   else
-	printf("Sequencia nao encontrada\n ");
+	else
+	{
+				MPI_Recv(buf, BUFSIZE, MPI_INT, 0, 11, MPI_COMM_WORLD, &status);
 
-   fclose(f);
-   return 0;
+				k=0;
+				for (i=0;i<n_seq;i++){
+							buff=seq_vet[i];
+							printf("i=%d buff %s\n ",i,buff);
+							for (j = 0; j < SEQ_SIZE-2; j++){
+									if (sequencia[j] != buff[j]){
+											break;
+								  }
+							}
+							if (j== SEQ_SIZE-2) {
+									indice_achou[k]=i;
+									k++;
+							}
+				}
+
+				MPI_Send(void *buf,int count,MPI_Datatype dtype,int dest,int tag,MPI_Comm comm);
+	}
+
+	fclose(f);
+
+	return 0;
 }
